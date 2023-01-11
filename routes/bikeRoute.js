0af069690967b6c14984396a8756f20dd6a55606
gopalkrishan <gopalkrishan.1997@gmail.com>
2023-01-11 21:16:58 +0530

    features added

--- routes/bikeRoute.js
+++ routes/bikeRoute.js
@@ -11,4 +11,41 @@ router.get("/getallbikes", async (req, res) => {
   }
 });
 
+router.post("/addbike", async (req, res) => {
+  try {
+    const newbike = new Bike(req.body);
+    await newbike.save();
+    res.send("Bike added successfully");
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+});
+
+router.post("/editbike", async (req, res) => {
+  try {
+    const bike = await Bike.findOne({ _id: req.body._id });
+    bike.name = req.body.name;
+    bike.image = req.body.image;
+    bike.fuelType = req.body.fuelType;
+    bike.rentPerHour = req.body.rentPerHour;
+    bike.capacity = req.body.capacity;
+
+    await bike.save();
+
+    res.send("Bike details updated successfully");
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+});
+
+router.post("/deletebike", async (req, res) => {
+  try {
+    await Bike.findOneAndDelete({ _id: req.body.bikeid });
+
+    res.send("Bike deleted successfully");
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -8,5 +8,8 @@ app.use(express.json())
 app.use('/api/bikes/' , require('./routes/bikeRoute'))
 app.use('/api/users/' , require('./routes/usersRoute'))
 
+app.use('/api/bookings/' , require('./routes/bookingsRoute'))
+
+
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
